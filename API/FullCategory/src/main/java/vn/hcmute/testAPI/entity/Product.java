d722c0f4d71047bac5da5f3ecf1b71c38d32532c
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2025-03-20 14:01:28 +0700

    api lastest product

--- API/FullCategory/src/main/java/vn/hcmute/testAPI/entity/Product.java
+++ API/FullCategory/src/main/java/vn/hcmute/testAPI/entity/Product.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -22,9 +23,73 @@ public class Product implements Serializable {
     private String productName;
     private String description;
     private double price;
+    private String image;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "categoryId")
     private Category category;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- API/FullCategory/src/main/java/vn/hcmute/testAPI/repository/ProductRepository.java
+++ API/FullCategory/src/main/java/vn/hcmute/testAPI/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package vn.hcmute.testAPI.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.hcmute.testAPI.entity.Product;
 
@@ -18,4 +19,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByProductNameContaining(String name, Pageable pageable);
     Optional<Product> findByProductName(String name);
     Optional<Product> findByProductId(Integer id);
+
+    @Query("SELECT p FROM Product p ORDER BY p.createdAt DESC LIMIT :limit")
+    List<Product> findLastestProducts(int limit);
 }
