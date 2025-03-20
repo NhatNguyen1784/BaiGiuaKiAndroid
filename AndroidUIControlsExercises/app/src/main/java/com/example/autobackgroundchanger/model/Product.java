2d7532daac31229a240ff8e532eac31a0b1fc6ac
vovan <vovantuan7702@gmail.com>
2025-03-20 14:08:45 +0700

    category

---
+++ AndroidUIControlsExercises/app/src/main/java/com/example/autobackgroundchanger/model/Product.java
@@ -0,0 +1,47 @@
+package com.example.autobackgroundchanger.model;
+
+public class Product {
+    private Long productId;
+    private String productName;
+    private String description;
+    private double price;
+
+    public Product(Long productId, String productName, String description, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
