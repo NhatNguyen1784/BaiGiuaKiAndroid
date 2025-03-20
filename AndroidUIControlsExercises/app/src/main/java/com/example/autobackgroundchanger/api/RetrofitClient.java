f37a8857646918720b66300347d79d4115a23a78
onlydaz <163791995+onlydaz@users.noreply.github.com>
2025-03-20 16:37:08 +0700

    fix

--- AndroidUIControlsExercises/app/src/main/java/com/example/autobackgroundchanger/api/RetrofitClient.java
+++ AndroidUIControlsExercises/app/src/main/java/com/example/autobackgroundchanger/api/RetrofitClient.java
@@ -2,6 +2,7 @@ package com.example.autobackgroundchanger.api;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
     private static final String BASE_URL = "http://10.0.2.2:8080/api/";
@@ -11,7 +12,8 @@ public class RetrofitClient {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create()) // Thêm GsonConverterFactory
+                    .addConverterFactory(ScalarsConverterFactory.create()) // Xử lý String
+                    .addConverterFactory(GsonConverterFactory.create()) // Xử lý JSON
                     .build();
         }
         return retrofit;
