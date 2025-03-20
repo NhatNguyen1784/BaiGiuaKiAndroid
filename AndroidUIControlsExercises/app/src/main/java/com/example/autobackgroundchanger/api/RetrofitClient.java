e4c85d4aca775c26ce1d1dcef7554ddcfcd06aa0
onlydaz <163791995+onlydaz@users.noreply.github.com>
2025-03-20 20:06:05 +0700

    update

--- AndroidUIControlsExercises/app/src/main/java/com/example/autobackgroundchanger/api/RetrofitClient.java
+++ AndroidUIControlsExercises/app/src/main/java/com/example/autobackgroundchanger/api/RetrofitClient.java
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
     private static final String BASE_URL = "http://10.0.2.2:8080/api/";
@@ -20,7 +21,8 @@ public class RetrofitClient {
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(ScalarsConverterFactory.create()) // Xử lý String
+                    .addConverterFactory(GsonConverterFactory.create()) // Xử lý JSON
                     .client(client)
                     .build();
         }
